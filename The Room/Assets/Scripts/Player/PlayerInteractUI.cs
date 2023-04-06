df26e966042872f7bb42eae0a80124a7c5b26e54
Ata Türkoğlu <ataturkoglu2003@hotmail.com>
2023-04-06 16:51:17 +0300

    Tweaks
    - Made UI animations better.
    - The enemy is now a prefab.
    - Added crosshair.

--- The Room/Assets/Scripts/Player/PlayerInteractUI.cs
+++ The Room/Assets/Scripts/Player/PlayerInteractUI.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -10,30 +11,42 @@ public class PlayerInteractUI : MonoBehaviour
     private CanvasGroup canvasGroup;
     private TMP_Text text;
 
+    private bool isHidden = true;
+
     private void Awake()
     {
         player = FindObjectOfType<PlayerInteractionManager>();
-        canvasGroup = GetComponentInParent<CanvasGroup>();
         text = GetComponentInChildren<TMP_Text>();
+        
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0f;
     }
 
     private void Update()
     {
-        text.text = $"Press \"E\" to {player.GetInteractionLabel()}";
-        if (player.ShouldDisplayInteractToggle())
+        var interactionLabel = player.GetInteractionLabel();
+        if (!string.IsNullOrWhiteSpace(interactionLabel))
+            text.text = $"Press \"E\" to {interactionLabel}";
+        
+        var newIsHidden = player.ShouldDisplayInteractToggle();
+
+        // We do these checks so we call the Show only when the UI is actually hidden and vice versa.
+        if (!isHidden && newIsHidden)
             Show();
-        else
+        else if (isHidden && !newIsHidden)
             Hide();
+        
+        isHidden = newIsHidden;
     }
     private void Show()
     {
         canvasGroup.DOFade(1f, FADE_DUR);
-        transform.DOScale(Vector3.one, FADE_DUR);
+        transform.DOPunchPosition(Vector3.down, FADE_DUR);
     }
 
     private void Hide()
     {
         canvasGroup.DOFade(0f, FADE_DUR);
-        transform.DOScale(Vector3.zero, FADE_DUR);
+        transform.DOPunchPosition(Vector3.up, FADE_DUR);
     }
 }
